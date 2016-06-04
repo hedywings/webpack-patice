0aa27a7ab6e459991f556a599abedfd39ba45f9b
Hedy Wang <hedywings@gmail.com>
2016-06-04 12:47:35 +0800

    ongoing

---
+++ build/style.b68b5216864f048da736.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],[function(n,c){}]);
+//# sourceMappingURL=style.b68b5216864f048da736.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,54 @@
 var path = require('path'),
-	HtmlWebpackPlugin = require('html-webpack-plugin');
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    merge = require('webpack-merge'),
+    validate = require('webpack-validator');
+
+var parts = require('./lib/parts');
 
 var PATHS = {
-	app: path.join(__dirname, 'app'),
-	build: path.join(__dirname, 'build')
+    app: path.join(__dirname, 'app'),
+    style: path.join(__dirname, 'app', 'main.css'),
+    build: path.join(__dirname, 'build')
 };
 
-module.exports = {
-	entry: {
-		app: PATHS.app
-	},
-	output: {
-		path: PATHS.build,
-		filename: '[name].js'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({ title: 'webpack demo' })
-	]
+var config = {};
+var common = {
+    entry: {
+        style: PATHS.style,
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].[hash].js',
+        chunkFilename: '[hash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ title: 'webpack demo' })
+    ]
 };
+
+switch (process.env.npm_lifecycle_event) {
+    case 'build':
+        config = merge(common, 
+            { devtool: 'source-map' },
+            parts.clean(PATHS.build),
+            parts.setFreeVariable('process.env.NODE_ENV', 'production'),
+            parts.extractBundle({ name: 'vendor', entries: ['react'] }),
+            parts.minify(),
+            // parts.setupCSS(PATHS.app),
+            parts.extractCSS(PATHS.style)
+        );
+        break;
+    default:
+        config = merge(common, 
+            { devtool: 'eval-source-map' },
+            parts.setupCSS(PATHS.app),
+            parts.extractCSS(PATHS.style),
+            parts.devServer({
+                host: process.env.HOST,
+                port: process.env.PORT
+            })
+        );
+}
+
+module.exports = validate(config);
