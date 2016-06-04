cd1ed314a8801188419bbba75c18a36d29ab52ae
Hedy Wang <hedywings@gmail.com>
2016-06-04 13:08:05 +0800

    Updates

---
+++ app.f8b7606f0e2f2859b211.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,3],{0:function(n,e,o){o(1);var t=o(34);document.body.appendChild(t())},34:function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Hello World",n}}});
+//# sourceMappingURL=app.f8b7606f0e2f2859b211.js.map
---
+++ style.f8b7606f0e2f2859b211.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],{0:function(n,o,c){c(35),n.exports=c(39)},35:function(n,o){},39:function(n,o){}});
+//# sourceMappingURL=style.f8b7606f0e2f2859b211.js.map
